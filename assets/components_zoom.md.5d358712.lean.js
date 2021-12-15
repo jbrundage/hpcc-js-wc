fc9bf626dec3618a320f7b8c0f6810fc0b27c905
GordonSmith <GordonSmith@users.noreply.github.com>
2021-12-15 05:48:59 +0000

    deploy: ca2e90b85d7b1694503f5c581a9ae5a52e828932

---
+++ assets/components_zoom.md.5d358712.lean.js
@@ -0,0 +1,16 @@
+import{_ as o,r as t,c as n,b as a,w as i,d as e,e as l,a as c,o as d}from"./app.3406d70a.js";const z='{"title":"Drag and Zoom","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":2,"title":"Properties","slug":"properties"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"components/zoom.md","lastUpdated":1639547278752}',u={},m=e("h1",{id:"drag-and-zoom",tabindex:"-1"},[l("Drag and Zoom "),e("a",{class:"header-anchor",href:"#drag-and-zoom","aria-hidden":"true"},"#")],-1),h=e("pre",null,[e("code",null,`<hpcc-zoom x=10 y=10 scale=0.63 width="100%" height="100%" style="flex: 1 0 auto;">
+  <h1>HTML Ipsum Presents</h1>
+  <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  <h2>Header Level 2</h2>
+  <ol>
+  <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+  <li>Aliquam tincidunt mauris eu risus.</li>
+  </ol>
+  <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+  <h3>Header Level 3</h3>
+  <ul>
+    <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+    <li>Aliquam tincidunt mauris eu risus.</li>
+  </ul>
+</hpcc-zoom>
+`)],-1),p=c("",14);function f(_,g,b,v,x,q){const s=t("hpcc-preview"),r=t("ClientOnly");return d(),n("div",null,[m,a(r,null,{default:i(()=>[a(s,{width:"100%",height:"400px",style:{"background-color":"pink"}},{default:i(()=>[h]),_:1})]),_:1}),p])}var V=o(u,[["render",f]]);export{z as __pageData,V as default};
