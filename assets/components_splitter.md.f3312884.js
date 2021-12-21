ef439f5e5d4e9229dcdf0508eedddd4ab4be154d
GordonSmith <GordonSmith@users.noreply.github.com>
2021-12-21 18:02:14 +0000

    deploy: 7eae994c0cdfad0741c1893cf3bde8f909c8ec5a

---
+++ assets/components_splitter.md.f3312884.js
@@ -0,0 +1,37 @@
+import{_ as u,r,c as o,b as t,w as i,d as e,e as s,a as l,o as c}from"./app.487bc5f8.js";const y='{"title":"Splitter","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"height","slug":"height"},{"level":3,"title":"width","slug":"width"},{"level":2,"title":"Events","slug":"events"},{"level":3,"title":"changed","slug":"changed"}],"relativePath":"components/splitter.md","lastUpdated":1640109679404}',m={},p=e("h1",{id:"splitter",tabindex:"-1"},[s("Splitter "),e("a",{class:"header-anchor",href:"#splitter","aria-hidden":"true"},"#")],-1),d=e("p",null,[e("strong",null,"tag"),s(": "),e("code",null,"<hpcc-hsplitter>")],-1),h=e("pre",null,[e("code",null,`<hpcc-hsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-hsplitter>
+`)],-1),g=e("p",null,[e("strong",null,"tag"),s(": "),e("code",null,"<hpcc-vsplitter>")],-1),f=e("pre",null,[e("code",null,`<hpcc-vsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-vsplitter>
+`)],-1),v=e("p",null,[e("strong",null,"tag"),s(": "),e("code",null,"<hpcc-hsplitter> + <hpcc-vsplitter> combined")],-1),b=e("pre",null,[e("code",null,`<hpcc-hsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+    </div>
+  <hpcc-vsplitter width="100%" height="400px" style="border:0px;padding:0px">
+      <div style="overflow:auto">
+        <h1>HTML Ipsum Presents</h1>
+        <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+      </div>
+      <div style="overflow:auto">
+        <h1>HTML Ipsum Presents</h1>
+        <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+  </hpcc-vsplitter>
+</hpcc-hsplitter>
+`)],-1),_=l('<div class="tip custom-block"><p class="custom-block-title">TIP</p><p>See <a href="./../guide/getting-started.html">Getting Started</a> for details on how to include @hpcc-js/web-components in your application</p></div><h2 id="attributes" tabindex="-1">Attributes <a class="header-anchor" href="#attributes" aria-hidden="true">#</a></h2><p>HPCCSplitterElement HPCCResizeElement HPCCElement</p><h3 id="height" tabindex="-1"><code>height</code> <a class="header-anchor" href="#height" aria-hidden="true">#</a></h3><p><i>The element height</i></p><p><strong>Type:</strong> <code>number | string</code></p><p><strong>Default Value:</strong></p><h3 id="width" tabindex="-1"><code>width</code> <a class="header-anchor" href="#width" aria-hidden="true">#</a></h3><p><i>The element width</i></p><p><strong>Type:</strong> <code>number | string</code></p><p><strong>Default Value:</strong></p><h2 id="events" tabindex="-1">Events <a class="header-anchor" href="#events" aria-hidden="true">#</a></h2><h3 id="changed" tabindex="-1"><code>changed</code> <a class="header-anchor" href="#changed" aria-hidden="true">#</a></h3><p><em>Emitted whenever the content is panned or zoomed</em></p>',14);function w(q,x,P,V,T,A){const a=r("hpcc-preview"),n=r("ClientOnly");return c(),o("div",null,[p,d,t(n,null,{default:i(()=>[t(a,{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},{default:i(()=>[h]),_:1})]),_:1}),g,t(n,null,{default:i(()=>[t(a,{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},{default:i(()=>[f]),_:1})]),_:1}),v,t(n,null,{default:i(()=>[t(a,{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},{default:i(()=>[b]),_:1})]),_:1}),_])}var C=u(m,[["render",w]]);export{y as __pageData,C as default};
