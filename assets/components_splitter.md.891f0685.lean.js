81209c17ecf9bb8a4442fbcd06bd3dbaee067d60
GordonSmith <GordonSmith@users.noreply.github.com>
2021-12-22 09:05:22 +0000

    deploy: e126b77d1e93e535b65d5a1891ae69b2fbc993a1

---
+++ assets/components_splitter.md.891f0685.lean.js
@@ -0,0 +1,37 @@
+import{_ as n,r,c as u,b as s,w as a,d as e,e as t,a as o,o as l}from"./app.487bc5f8.js";const D='{"title":"Splitter","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"height","slug":"height"},{"level":3,"title":"width","slug":"width"},{"level":2,"title":"Events","slug":"events"},{"level":3,"title":"changed","slug":"changed"}],"relativePath":"components/splitter.md","lastUpdated":1640163863779}',c={},m=e("h1",{id:"splitter",tabindex:"-1"},[t("Splitter "),e("a",{class:"header-anchor",href:"#splitter","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-hsplitter>")],-1),d=e("hpcc-preview",{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},[e("pre",null,[e("code",null,`<hpcc-hsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-hsplitter>
+`)])],-1),h=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-vsplitter>")],-1),g=e("hpcc-preview",{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},[e("pre",null,[e("code",null,`<hpcc-vsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-vsplitter>
+`)])],-1),v=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-hsplitter> + <hpcc-vsplitter> combined")],-1),f=e("hpcc-preview",{previewBorder:"0px",width:"100%",height:"400px",fullReload:""},[e("pre",null,[e("code",null,`<hpcc-hsplitter width="100%" height="400px">
+  <div style="overflow:auto">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <hpcc-vsplitter width="100%" height="100%" style="border:0px;padding:0px">
+    <div style="overflow:auto">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+    <div style="overflow:auto">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+  </hpcc-vsplitter>
+</hpcc-hsplitter>
+`)])],-1),b=o("",14);function _(w,q,x,P,V,T){const i=r("ClientOnly");return l(),u("div",null,[m,p,s(i,null,{default:a(()=>[d]),_:1}),h,s(i,null,{default:a(()=>[g]),_:1}),v,s(i,null,{default:a(()=>[f]),_:1}),b])}var y=n(c,[["render",_]]);export{D as __pageData,y as default};
