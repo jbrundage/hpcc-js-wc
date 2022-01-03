d88fda43983ad4e451e5da2a08b794cbbf42f820
Gordon Smith <GordonJSmith@gmail.com>
2022-01-03 09:32:37 +0000

    WIP
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ assets/components_codemirror.md.0fa40368.lean.js
@@ -0,0 +1,11 @@
+import{_ as r,r as d,c as i,b as a,w as n,d as e,e as t,a as c,o as h}from"./app.84e934ba.js";const y='{"title":"CodeMirror Editor","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"height","slug":"height"},{"level":3,"title":"width","slug":"width"},{"level":3,"title":"content","slug":"content"},{"level":3,"title":"mode","slug":"mode"},{"level":3,"title":"theme","slug":"theme"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"components/codemirror.md","lastUpdated":1639814527783}',s={},l=e("h1",{id:"codemirror-editor",tabindex:"-1"},[t("CodeMirror Editor "),e("a",{class:"header-anchor",href:"#codemirror-editor","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-codemirror>")],-1),u=e("hpcc-preview",{width:"100%",height:"404px"},[e("pre",null,[e("code",null,`  <hpcc-codemirror mode="json" theme="light" width="100%" height="400px"></hpcc-codemirror>
+  <script>
+      document.querySelector('hpcc-codemirror').text = \`\\
+{
+  "aaa":123, 
+  "bbb":"ddd", 
+  "c":3, 
+  "d":true
+}\`;
+  <\/script>
+`)])],-1),g=c("",25);function m(_,b,f,v,x,T){const o=d("ClientOnly");return h(),i("div",null,[l,p,a(o,null,{default:n(()=>[u]),_:1}),g])}var V=r(s,[["render",m]]);export{y as __pageData,V as default};
