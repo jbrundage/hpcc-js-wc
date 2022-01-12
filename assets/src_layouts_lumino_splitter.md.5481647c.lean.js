8616cde6eb797175e013dcef1dab1ac502832759
GordonSmith <GordonSmith@users.noreply.github.com>
2022-01-12 08:15:26 +0000

    deploy: 1c159b28ba8e80dd67c126cf319f7d1b32b509fd

---
+++ assets/src_layouts_lumino_splitter.md.5481647c.lean.js
@@ -0,0 +1,37 @@
+import{_ as n,r,c as o,e as a,w as s,b as e,d as t,a as l,o as u}from"./app.680dc6a2.js";const T='{"title":"Splitter","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"orientation","slug":"orientation"},{"level":3,"title":"spacing","slug":"spacing"},{"level":2,"title":"Events","slug":"events"},{"level":3,"title":"changed","slug":"changed"},{"level":2,"title":"More Examples","slug":"more-examples"},{"level":3,"title":"Vertical","slug":"vertical"},{"level":3,"title":"Horizontal and vertical","slug":"horizontal-and-vertical"}],"relativePath":"src/layouts/lumino/splitter.md","lastUpdated":1641975257604}',c={},m=e("h1",{id:"splitter",tabindex:"-1"},[t("Splitter "),e("a",{class:"header-anchor",href:"#splitter","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-splitter>")],-1),d=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitter orientation="horizontal" style="width:100%;height:100%">
+  <div style="overflow:auto;min-width:48px">
+    <h1>AAA Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto;min-width:48px">
+    <h1>BBB Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-splitter>
+`)])],-1),h=l("",17),g=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitter orientation="vertical" style="width:100%;height:100%">
+  <div style="overflow:auto;min-height:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto;min-height:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-splitter>
+`)])],-1),v=e("h3",{id:"horizontal-and-vertical",tabindex:"-1"},[t("Horizontal and vertical "),e("a",{class:"header-anchor",href:"#horizontal-and-vertical","aria-hidden":"true"},"#")],-1),f=e("p",null,[e("strong",null,"tag:"),t(),e("code",null,"<hpcc-splitter>"),t(" and "),e("code",null,'<hpcc-splitter orientation="vertical">')],-1),b=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitter orientation="horizontal" style="width:100%;height:100%">
+  <div style="overflow:auto;min-width:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <hpcc-splitter orientation="vertical" width="100%" height="100%" style="border:0px;padding:0px;min-width:48px">
+    <div style="overflow:auto;min-height:48px">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+    <div style="overflow:auto;min-height:48px">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+  </hpcc-splitter>
+</hpcc-splitter>
+`)])],-1);function _(w,q,x,V,y,A){const i=r("ClientOnly");return u(),o("div",null,[m,p,a(i,null,{default:s(()=>[d]),_:1}),h,a(i,null,{default:s(()=>[g]),_:1}),v,f,a(i,null,{default:s(()=>[b]),_:1})])}var D=n(c,[["render",_]]);export{T as __pageData,D as default};
