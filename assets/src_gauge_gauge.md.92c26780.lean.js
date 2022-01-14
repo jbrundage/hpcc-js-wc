2ab25dc66bbf344d3b0606c72252285e3dde56a5
GordonSmith <GordonSmith@users.noreply.github.com>
2022-01-14 10:38:45 +0000

    deploy: 654b566023f9b8e0d8ca38d4abc37d1630bbbb01

---
+++ assets/src_gauge_gauge.md.92c26780.lean.js
@@ -0,0 +1,17 @@
+import{_ as a,r as i,c as r,e as l,w as n,b as e,d as t,a as c,o as s}from"./app.12365119.js";const T='{"title":"Gauge","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"colorDomain","slug":"colordomain"},{"level":3,"title":"colorRange","slug":"colorrange"},{"level":3,"title":"emptyColor","slug":"emptycolor"},{"level":3,"title":"showTick","slug":"showtick"},{"level":3,"title":"tickColor","slug":"tickcolor"},{"level":3,"title":"tickValue","slug":"tickvalue"},{"level":3,"title":"tickValueDescription","slug":"tickvaluedescription"},{"level":3,"title":"title","slug":"title"},{"level":3,"title":"titleDescription","slug":"titledescription"},{"level":3,"title":"tooltip","slug":"tooltip"},{"level":3,"title":"value","slug":"value"},{"level":3,"title":"valueDescription","slug":"valuedescription"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"src/gauge/gauge.md","lastUpdated":1642156630689}',d={},u=e("h1",{id:"gauge",tabindex:"-1"},[t("Gauge "),e("a",{class:"header-anchor",href:"#gauge","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-gauge>")],-1),h=e("hpcc-preview",{previewBorder:"0px",previewHeightRatio:"0.33",style:{width:"100%",height:"400px"}},[e("pre",null,[e("code",null,`<hpcc-splitPanel style="width:100%;height:100%">
+  <hpcc-gauge showTick title="G-01" titleDescription="Gauge 01" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge title="G-02" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge showTick title="G-03" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge title="G-04" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+</hpcc-splitPanel>
+<script>
+  for (const gauge of document.querySelectorAll("hpcc-gauge")) {
+    gauge.value = Math.random();
+    gauge.tickValue = Math.random();
+  }
+<\/script>
+`)])],-1),g=c("",51);function v(f,m,q,y,_,k){const o=i("ClientOnly");return s(),r("div",null,[u,p,l(o,null,{default:n(()=>[h]),_:1}),g])}var w=a(d,[["render",v]]);export{T as __pageData,w as default};
---
+++ assets/src_layouts_lumino_splitPanel.md.43b44a1b.lean.js
@@ -0,0 +1,37 @@
+import{_ as n,r,c as o,e as a,w as s,b as e,d as t,a as l,o as u}from"./app.12365119.js";const T='{"title":"Split Panel","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"orientation","slug":"orientation"},{"level":3,"title":"spacing","slug":"spacing"},{"level":2,"title":"Events","slug":"events"},{"level":3,"title":"changed","slug":"changed"},{"level":2,"title":"More Examples","slug":"more-examples"},{"level":3,"title":"Vertical","slug":"vertical"},{"level":3,"title":"Horizontal and vertical","slug":"horizontal-and-vertical"}],"relativePath":"src/layouts/lumino/splitPanel.md","lastUpdated":1642156630689}',c={},p=e("h1",{id:"split-panel",tabindex:"-1"},[t("Split Panel "),e("a",{class:"header-anchor",href:"#split-panel","aria-hidden":"true"},"#")],-1),m=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-splitpanel>")],-1),d=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitpanel orientation="horizontal" style="width:100%;height:100%">
+  <div style="overflow:auto;min-width:48px">
+    <h1>AAA Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto;min-width:48px">
+    <h1>BBB Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-splitpanel>
+`)])],-1),h=l("",17),g=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitpanel orientation="vertical" style="width:100%;height:100%">
+  <div style="overflow:auto;min-height:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <div style="overflow:auto;min-height:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+</hpcc-splitpanel>
+`)])],-1),v=e("h3",{id:"horizontal-and-vertical",tabindex:"-1"},[t("Horizontal and vertical "),e("a",{class:"header-anchor",href:"#horizontal-and-vertical","aria-hidden":"true"},"#")],-1),f=e("p",null,[e("strong",null,"tag:"),t(),e("code",null,"<hpcc-splitpanel>"),t(" and "),e("code",null,'<hpcc-splitpanel orientation="vertical">')],-1),b=e("hpcc-preview",{previewBorder:"0px",style:{width:"100%",height:"600px"}},[e("pre",null,[e("code",null,`<hpcc-splitpanel orientation="horizontal" style="width:100%;height:100%">
+  <div style="overflow:auto;min-width:48px">
+    <h1>HTML Ipsum Presents</h1>
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+  </div>
+  <hpcc-splitpanel orientation="vertical" width="100%" height="100%" style="border:0px;padding:0px;min-width:48px">
+    <div style="overflow:auto;min-height:48px">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+    <div style="overflow:auto;min-height:48px">
+      <h1>HTML Ipsum Presents</h1>
+      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+    </div>
+  </hpcc-splitpanel>
+</hpcc-splitpanel>
+`)])],-1);function _(w,q,x,V,P,y){const i=r("ClientOnly");return u(),o("div",null,[p,m,a(i,null,{default:s(()=>[d]),_:1}),h,a(i,null,{default:s(()=>[g]),_:1}),v,f,a(i,null,{default:s(()=>[b]),_:1})])}var D=n(c,[["render",_]]);export{T as __pageData,D as default};
