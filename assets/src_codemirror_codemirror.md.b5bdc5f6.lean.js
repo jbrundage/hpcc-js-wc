f7ab52c2eceae4d7765d2cb2facdb760ecc49d52
GordonSmith <GordonSmith@users.noreply.github.com>
2022-01-14 10:11:05 +0000

    deploy: 7180c9706ee30e3123d60a1f0db2b2148bf37051

---
+++ assets/src_codemirror_codemirror.md.b5bdc5f6.lean.js
@@ -0,0 +1,12 @@
+import{_ as r,r as d,c as i,e as a,w as c,b as e,d as t,a as n,o as s}from"./app.12365119.js";const q='{"title":"CodeMirror Editor","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"mode","slug":"mode"},{"level":3,"title":"text","slug":"text"},{"level":3,"title":"theme","slug":"theme"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"src/codemirror/codemirror.md","lastUpdated":1642154970010}',l={},h=e("h1",{id:"codemirror-editor",tabindex:"-1"},[t("CodeMirror Editor "),e("a",{class:"header-anchor",href:"#codemirror-editor","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-codemirror>")],-1),u=e("hpcc-preview",{previewBorder:"0px",previewHeightRatio:"0.5",style:{width:"100%",height:"400px"}},[e("pre",null,[e("code",null,`  <hpcc-codemirror mode="json" theme="dark" style="width:100%;height:100%">
+  </hpcc-codemirror>
+  <script>
+      document.querySelector('hpcc-codemirror').text = \`\\
+{
+  "aaa":123, 
+  "bbb":"ddd", 
+  "c":3, 
+  "d":true
+}\`;
+  <\/script>
+`)])],-1),m=n("",17);function _(g,x,b,f,v,T){const o=d("ClientOnly");return s(),i("div",null,[h,p,a(o,null,{default:c(()=>[u]),_:1}),m])}var w=r(l,[["render",_]]);export{q as __pageData,w as default};
