a04db3bf5537533bd6c0407f925f1738ed5289ce
GordonSmith <GordonSmith@users.noreply.github.com>
2022-01-14 10:46:47 +0000

    deploy: 296f97c07f6b5a63cc452d5fd9c0fa3cbb8ce207

---
+++ assets/CONTRIBUTING.md.ee590e83.lean.js
@@ -0,0 +1,5 @@
+import{_ as n,c as s,o,b as e,d as t}from"./app.12365119.js";const f='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"CONTRIBUTING.md","lastUpdated":1642157092724}',c={},r=e("p",null,"To start the component development environment:",-1),a=e("div",{class:"language-"},[e("pre",null,[e("code",null,`git clone https://github.com/GordonSmith/hpcc-js-wc.git
+cd hpcc-js-wc
+npm install
+npm run serve
+`)])],-1),l=e("p",null,[t("In vscode pressing "),e("code",null,"ctrl+shift+b"),t(" will start the build and serve process. Pressing "),e("code",null,"F5"),t(" will start debugging in the browser.")],-1),d=[r,a,l];function i(p,_,h,u,m,g){return o(),s("div",null,d)}var b=n(c,[["render",i]]);export{f as __pageData,b as default};
